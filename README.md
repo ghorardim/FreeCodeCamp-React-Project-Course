8ca851c082bd0771350b9e19c1eb7b5391a0062b
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-09-26 16:47:05 +0600

    Done: 1:03:20 learn props, hook, passing data from parent to child

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FreeCodeCamp-React-Project-Course
+
+course link: https://www.youtube.com/watch?v=u6gSSpfsoOQ&t=1509s
--- team-member-allocation/src/Employees.jsx
+++ team-member-allocation/src/Employees.jsx
@@ -1,8 +1,67 @@
-import { useState } from "react";
-const Employees = () => {
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
+
+const Employees = ({
+  employees,
+  selectedTeam,
+  handleTeamSelectionChange,
+  handleEmployeeCardClick,
+}) => {
   return (
-    <main>
-        
+    <main className="container">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">Team A</option>
+            <option value="TeamB">Team B</option>
+            <option value="TeamC">Team C</option>
+            <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-8">
+          <div className="card-collection">
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className={
+                  employee.teamName === selectedTeam
+                    ? "card m-2 standout"
+                    : "card m-2"
+                }
+                style={{ cursor: "pointer" }}
+                key={employee.id}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img
+                    src={maleProfile}
+                    className="card-img-top"
+                    alt="profile images"
+                  />
+                ) : (
+                  <img
+                    src={femaleProfile}
+                    className="card-img-top"
+                    alt="profile images"
+                  />
+                )}
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b> {employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
