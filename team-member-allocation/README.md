c8ba9f859ff675d3e1fe66b77892ff0709dbd4d2
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-09-28 09:46:26 +0600

    devide Employee component

--- team-member-allocation/README.md
+++ team-member-allocation/README.md
@@ -1,4 +1,6 @@
 Instructor: Gavin Lon
+
+Instructor Code: https://github.com/GavinLonDigital/TeamMemberAllocationApp
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ team-member-allocation/src/TeamMembers.jsx
@@ -0,0 +1,14 @@
+import TeamMemberCard from './TeamMemberCard';
+
+const TeamMembers = ({ employees, selectedTeam, handleEmployeeCardClick }) => {
+  return employees.map((employee) => (
+    <TeamMemberCard
+      key = {employee.id}
+      employee={employee}
+      selectedTeam={selectedTeam}
+      handleEmployeeCardClick={handleEmployeeCardClick}
+    />
+  ));
+};
+
+export default TeamMembers;
