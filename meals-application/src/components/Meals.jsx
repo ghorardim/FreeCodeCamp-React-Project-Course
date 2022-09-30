8af507ae94dbc797859099e9b743aad32da23f2a
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-09-30 18:00:02 +0600

    completed 03:03:11, style meals component

--- meals-application/src/components/Meals.jsx
+++ meals-application/src/components/Meals.jsx
@@ -1,8 +1,28 @@
 import { useGlobalContext } from '../context';
 const Meals = () => {
-  const context = useGlobalContext();
-  console.log(context);
-  return <h1>Hello World</h1>;
+  const { meals } = useGlobalContext();
+  console.log(meals);
+  return (
+    <section className="section-center">
+      {meals.map((item) => {
+        const { idMeal, strMeal: title, strMealThumb: image } = item;
+        return (
+          <article key={idMeal} className="single-meal">
+            <img
+              src={image}
+              style={{ width: '200px' }}
+              className="img"
+              alt={title + 'image'}
+            />
+            <footer>
+              <h5>{title}</h5>
+              <button className="like-btn">click me</button>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Meals;
--- meals-application/src/context.jsx
+++ meals-application/src/context.jsx
@@ -1,16 +1,17 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 const AppContext = React.createContext();
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=s';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({ children }) => {
+  const [meals, setMeals] = useState([]);
   const fetchMeals = async (url) => {
     try {
-      const response = await axios(url);
-      const {data} = response;
-      console.log(data);
+      const response = await axios.get(url);
+      const { data } = response;
+      setMeals(data.meals);
     } catch (e) {
       console.log(e.response);
     }
@@ -18,10 +19,8 @@ const AppProvider = ({ children }) => {
   useEffect(() => {
     fetchMeals(allMealsUrl);
   }, []);
-  return ( 
-    <AppContext.Provider value={{ allMealsUrl }}>
-      {children}
-    </AppContext.Provider>
+  return (
+    <AppContext.Provider value={{ meals }}>{children}</AppContext.Provider>
   );
 };
 
