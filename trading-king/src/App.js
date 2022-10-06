b75a8b707fffcf47e4fcbe161f3ac30b5eb1a16c
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-10-06 10:59:03 +0600

    implementing routing for two pages

--- trading-king/src/App.js
+++ trading-king/src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import { StockDetailPage } from "./pages/StockDetailPage";
+import { StockOverviewPage } from "./pages/StockOverviewPage";
 function App() {
   return (
     <main>
-      <h1 className="m-4">Hellow world</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StockOverviewPage/>}/>
+        <Route path="/detail/:symbol" element={<StockDetailPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </main>
   );
 }
