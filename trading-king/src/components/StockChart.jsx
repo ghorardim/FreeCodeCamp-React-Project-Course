f74ce3235777f596dea81a44e1271110024a83a5
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-10-11 17:27:15 +0600

    implement apex chart to display data

--- trading-king/src/components/StockChart.jsx
+++ trading-king/src/components/StockChart.jsx
@@ -1,8 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Chart from 'react-apexcharts';
 
-export default function StockChart({chartData,symbol}) {
-    const {day,week,year} = chartData;
+export default function StockChart({ chartData, symbol }) {
+  const [dateFormat, setDateFormat] = useState('24h');
+  const { day, week, year } = chartData;
+  const [color, setColor] = useState('');
+
+  const determineTimeFormat = () => {
+    switch (dateFormat) {
+      case '24h':
+        return day;
+      case '7d':
+        return week;
+      case '1y':
+        return year;
+      default:
+        return day;
+    }
+  };
+
+  const handleColor = () => {
+    let data = determineTimeFormat(dateFormat);
+    return data.length > 0 && data[data.length - 1].y - data[0].y > 0 ? '#26C281' : '#ed3419';
+  };
+
+  const options = {
+    colors: [color],
+    title: {
+      text: symbol,
+      align: 'center',
+      style: {
+        fontSize: '24px',
+      },
+    },
+    chart: {
+      id: 'stock data',
+      animations: {
+        speed: 1300,
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+      labels: {
+        datetimeUTC: true,
+      },
+    },
+    tooltip: {
+      x: {
+        format: 'MMM dd HH:MM',
+      },
+    },
+  };
+  const series = [
+    {
+      name: symbol,
+      data: determineTimeFormat(),
+    },
+  ];
+
+  const renderButtonSelect = (button) => {
+    const classes = 'btn m-1 ';
+    if (button === dateFormat) {
+      return classes + 'btn-primary';
+    } else {
+      return classes + 'btn-outline-primary';
+    }
+  };
+
+  const handleDataView = (dateFormat) => {
+    setDateFormat(dateFormat);
+    setColor(handleColor());
+  };
+
+  useEffect(() => {
+    if (day) setColor(handleColor());
+  }, [day]);
+
   return (
-    <div>StockChart</div>
-  )
+    <div className="mt-5 p-4 shadow-sm bg-white">
+      <Chart options={options} series={series} type="area" width="100%" />
+      <div>
+        <button className={renderButtonSelect('24h')} onClick={() => handleDataView('24h')}>
+          24h
+        </button>
+        <button className={renderButtonSelect('7d')} onClick={() => handleDataView('7d')}>
+          7d
+        </button>
+        <button className={renderButtonSelect('1y')} onClick={() => handleDataView('1y')}>
+          1y
+        </button>
+      </div>
+    </div>
+  );
 }
