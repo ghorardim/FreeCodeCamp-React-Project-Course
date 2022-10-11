e2e6272fd8502d7ebb994e3364a0a24e41c95249
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-10-11 10:05:54 +0600

    formate chart data and pass data to StockChart componet

---
+++ trading-king/src/components/StockChart.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function StockChart({chartData,symbol}) {
+    const {day,week,year} = chartData;
+  return (
+    <div>StockChart</div>
+  )
+}
--- trading-king/src/pages/StockDetailPage.jsx
+++ trading-king/src/pages/StockDetailPage.jsx
@@ -1,8 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import finnHub from '../apis/finnHub';
+import StockChart from '../components/StockChart';
+
+const formatData = (data) => {
+  return data.t.map((time, index) => {
+    return {
+      x: time * 1000,
+      y: Math.floor(data.c[index]),
+    };
+  });
+};
 
 export const StockDetailPage = () => {
+  const [chartData, setChartData] = useState({});
   const { symbol } = useParams();
   useEffect(() => {
     const fetchData = async () => {
@@ -46,9 +57,22 @@ export const StockDetailPage = () => {
           }),
         ]);
         console.log(responses);
+        setChartData({
+          day: formatData(responses[0].data),
+          week: formatData(responses[1].data),
+          year: formatData(responses[2].data),
+        });
       } catch (error) {}
     };
     fetchData();
-  }, []);
-  return <div>StockDetailPage</div>;
+  }, [symbol]);
+  return (
+    <div>
+      {chartData && (
+        <div>
+          <StockChart chartData={chartData} symbol={symbol}/>
+        </div>
+      )}
+    </div>
+  );
 };
